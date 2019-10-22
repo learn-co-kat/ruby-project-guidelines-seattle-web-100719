b9f20543342fc56fa7ba5aa705d0e8467ddcec5c
learn-co-kat <twothistles@gmail.com>
2019-10-22 16:44:30 -0700

    Fixed seeds file. All books created correctly.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,18 +13,9 @@ booklist.each do |row|
     number_of_pages = row["Number of Pages"]
     year_published = row["Year Published"]
     shelf = row["Exclusive Shelf"]
-    # date_added = row["Date Added"]
     
     Book.create(title: title, author: author, average_rating: average_rating, publisher: publisher, number_of_pages: number_of_pages, year_published: year_published, shelf: shelf)
 
 end
-
-# all_my_books.each do |book|
-#     title = book["volumeInfo"]["title"]
-#     published_date = book["volumeInfo"]["publishedDate"]
-#     desc = book["volumeInfo"]["description"]
-#     Book.create(title: title, published_date: published_date , description: desc)
-#     # binding.pry
-#   end
 
 binding.pry
