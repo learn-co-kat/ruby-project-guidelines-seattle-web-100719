09fb6f1ef031ec3a67fa9f1654753ba34ba051a2
learn-co-kat <twothistles@gmail.com>
2019-10-23 15:56:15 -0700

    Created method to create new reading listsssssssssss.

--- app/models/reading_list.rb
+++ app/models/reading_list.rb
@@ -3,6 +3,8 @@ class ReadingList < ActiveRecord::Base
 belongs_to :book
 belongs_to :reader
 
-self.create
+def self.new_list(name, author, book)
+ReadingList.create(name: name, author: author, book: book)
+end 
 
 end 
