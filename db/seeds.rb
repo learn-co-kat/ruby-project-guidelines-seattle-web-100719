b5c383032b8d828d4ba13c634339c3f90c13406d
learn-co-kat <twothistles@gmail.com>
2019-10-22 17:04:59 -0700

    Added readers

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,6 @@ require 'csv'
 require 'pry'
 
 csv_text = File.read("./db/goodreads_library_books.csv")
-
 booklist = CSV.parse(csv_text, :headers => true)
 
 booklist.each do |row|
@@ -15,7 +14,15 @@ booklist.each do |row|
     shelf = row["Exclusive Shelf"]
     
     Book.create(title: title, author: author, average_rating: average_rating, publisher: publisher, number_of_pages: number_of_pages, year_published: year_published, shelf: shelf)
-
 end
+
+csv_reader = File.read("./db/reader_names.csv")
+readerlist = CSV.parse(csv_reader, :headers => true)
+
+readerlist.each do |row|
+    name = row["Name"]
+
+    Reader.create(name: name)
+end 
 
 binding.pry
